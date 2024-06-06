4d7f5a2066e8db19fec67c1459cb19257cefbd85
Xel Lu <xellu@duck.com>
2024-06-06 12:03:50 +0700

    round 2

--- front/src/lib/sounds.ts
+++
@@ -1,7 +0,0 @@
-export function playSound(sound: string, volume: number = 1) {
-    console.info(`Playing sound: ${sound}`)
-
-    let audio = new Audio(`/${sound}`);
-    audio.volume = volume;
-    audio.play();
-}
