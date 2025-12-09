5afcd5ffea16d81943c081ea3272c08b92ae2f8d
Xel Lu <xellu@duck.com>
2025-12-09 17:30:21 +0700

    updated socials

--- front/src/lib/socials.ts
+++ front/src/lib/socials.ts
@@ -1,4 +1,4 @@
-export const instagram = "https://www.instagram.com/pajko0"
+export const instagram = "https://www.instagram.com/xelluuu/"
 export const github = "https://github.com/xellu"
 export const youtube = "https://youtube.com/@xellu"
 export const discord = "https://discord.com/users/923528901219729430"
